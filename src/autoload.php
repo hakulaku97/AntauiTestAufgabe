a7fb981d475ad01dcb3d6b95e63d518a4341d9cb
hakulaku97 <philipp.wiedemann@hotmail.com>
2025-08-11 17:43:52 +0200

    fixed autoload.php and Database.php

--- src/autoload.php
+++ src/autoload.php
@@ -3,8 +3,18 @@
 namespace Test;
 
 spl_autoload_register(function ($class) {
-    if(substr($class, 0, strlen(__NAMESPACE__)) != __NAMESPACE__ && substr($class, 0, strlen('\\'.__NAMESPACE__)) != '\\'.__NAMESPACE__)
+    $condition = substr($class, 0, strlen(__NAMESPACE__)) != __NAMESPACE__ &&
+        substr($class, 0, strlen('\\'.__NAMESPACE__)) != '\\'.__NAMESPACE__;
+
+    if($condition) {
         return false;
+    }
+
     $str_path = realpath(__DIR__.DIRECTORY_SEPARATOR.str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php');
-    if(file_exists($str_path)) require_once $str_path;
+
+    if(file_exists($str_path)){
+        require_once $str_path;
+    }
+
+    return true;
 });
